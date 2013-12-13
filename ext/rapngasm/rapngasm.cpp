ffdd22d801b971fa7b535da39e58a6965ad37301
Rika Yoshida <nazuki@genshin.org>
2013-12-13 16:16:57 +0900

    [wip] refactor rapngasm

--- ext/rapngasm/rapngasm.cpp
+++ ext/rapngasm/rapngasm.cpp
@@ -43,6 +43,16 @@ public:
 // }
 
 template<>
+Object to_ruby< unsigned char* > (unsigned char* const & x)
+{
+  unsigned char* const c = x;
+  Array a;
+  for (int i = 0; i < sizeof(c); i++)
+    a.push(c[i]);
+  return a;
+}
+
+template<>
 Object to_ruby<std::vector<APNGFrame> > (std::vector<APNGFrame> const & x)
 {
   std::vector<APNGFrame> v = x;
@@ -63,13 +73,13 @@ void Init_rapngasm()
       .define_method("width", &APNGFrame::width, (Arg("width") = 0))
       .define_method("height", &APNGFrame::height, (Arg("height") = 0))
       .define_method("color_type", &APNGFrame::colorType, (Arg("color_type") = 255))
-      .define_method("palette", &APNGFrame::palette, (Arg("palette") = NULL))
+      // .define_method("palette", &APNGFrame::palette, (Arg("palette") = NULL))
       .define_method("transparency", &APNGFrame::transparency, (Arg("transparency") = NULL))
       .define_method("palettes_size", &APNGFrame::paletteSize, (Arg("palettes_size") = 0))
       .define_method("transparency_size", &APNGFrame::transparencySize, (Arg("transparency_size") = NULL))
       .define_method("delay_numerator", &APNGFrame::delayNum, (Arg("delay_numerator") = 0))
       .define_method("delay_denominator", &APNGFrame::delayDen, (Arg("delay_denominator") = 0))
-      .define_method("rows", &APNGFrame::rows, (Arg("rows") = NULL));
+      // .define_method("rows", &APNGFrame::rows, (Arg("rows") = NULL));
 
     define_class<APNGAsm>("APNGAsm")
       .define_method("version", &APNGAsm::version)
