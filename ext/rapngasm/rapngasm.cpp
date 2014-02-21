aa109211014d2282a1d22e1111b8901c36cb1805
Rika Yoshida <nazuki@genshin.org>
2014-02-21 14:11:52 +0900

    [wip] fixed miss spell

--- ext/rapngasm/rapngasm.cpp
+++ ext/rapngasm/rapngasm.cpp
@@ -16,25 +16,25 @@ using namespace apngasm;
 
 namespace apngasm
 {
-class RAPNGAsm : public APNGAsm
-{
-  public:
-    size_t addFrameFromFrameObject(const APNGFrame &frame)
-    {
-      return this->addFrame(frame);
-    }
+  class RAPNGAsm : public APNGAsm
+  {
+    public:
+      size_t addFrameFromFrameObject(const APNGFrame &frame)
+      {
+        return this->addFrame(frame);
+      }
 
-    size_t addFrameFromFile(const std::string &filePath,
-                            unsigned delayNum = DEFAULT_FRAME_NUMERATOR, unsigned delayDen = DEFAULT_FRAME_DENOMINATOR)
-    {
-      return this->addFrame(filePath, delayNum, delayDen);
-    }
+      size_t addFrameFromFile(const std::string &filePath,
+                              unsigned delayNum = DEFAULT_FRAME_NUMERATOR, unsigned delayDen = DEFAULT_FRAME_DENOMINATOR)
+      {
+        return this->addFrame(filePath, delayNum, delayDen);
+      }
 
-    template<typename T>
-    T from_ruby(Object o);
-    
-    template<typename T>
-    Object to_ruby(T const & x);
+      template<typename T>
+      T from_ruby(Object o);
+
+      template<typename T>
+      Object to_ruby(T const & x);
   };
 }
 
@@ -103,8 +103,9 @@ rgb* from_ruby< rgb* > (Object o)
   for (unsigned int i = 0; i < a.size(); i++)
   {
       Hash h = (Hash)a[i];
-      rgb r = { (int)from_ruby<unsigned char>(h[Symbol("r")]), (int)from_ruby<unsigned char>(h[Symbol("g")]),
-                (int)from_ruby<unsigned char>(h[Symbol("b")]) };
+      rgb r = { (int)from_ruby<unsigned char>(h[Symbol("r")]), (int)from_ruby<unsigned char>(h[Symbol("g")]), (int)from_ruby<unsigned char>(h[Symbol("b")]) };
+      // Array array(a[i]);
+      // rgb r = { (int)from_ruby<unsigned char>(array[0]), (int)from_ruby<unsigned char>(array[1]), (int)from_ruby<unsigned char>(array[2]) };
       rgbArray[i] = r;
   }
   return rgbArray;
@@ -132,16 +133,16 @@ void Init_rapngasm()
     define_class<APNGFrame>("APNGFrame")
       //.define_constructor(Constructor<APNGFrame>())
       .define_constructor(Constructor<APNGFrame, const std::string, unsigned, unsigned>(),
-                        (Arg("file_path"), Arg("delay_num") = DEFAULT_FRAME_NUMERATOR, Arg("dela_den") = DEFAULT_FRAME_DENOMINATOR))
+                       (Arg("file_path"), Arg("delay_num") = DEFAULT_FRAME_NUMERATOR, Arg("delay_den") = DEFAULT_FRAME_DENOMINATOR))
       // .define_constructor(Constructor<APNGFrame, rgb*, unsigned int, unsigned int, unsigned, unsigned>(),
       //                    (Arg("pixels"), Arg("width"), Arg("height"),
-      //                     Arg("delay_num") = DEFAULT_FRAME_NUMERATOR, Arg("dela_den") = DEFAULT_FRAME_DENOMINATOR))
+      //                     Arg("delay_num") = DEFAULT_FRAME_NUMERATOR, Arg("delay_den") = DEFAULT_FRAME_DENOMINATOR))
       // .define_constructor(Constructor<APNGFrame, rgb*, unsigned int, unsigned int, rgb*, unsigned, unsigned>(),
       //                    (Arg("pixels"), Arg("width"), Arg("height"), Arg("trns_color") = NULL,
-      //                     Arg("delay_num") = DEFAULT_FRAME_NUMERATOR, Arg("dela_den") = DEFAULT_FRAME_DENOMINATOR))
+      //                     Arg("delay_num") = DEFAULT_FRAME_NUMERATOR, Arg("delay_den") = DEFAULT_FRAME_DENOMINATOR))
       // .define_constructor(Constructor<APNGFrame, rgba*, unsigned int, unsigned int, unsigned, unsigned>(),
       //                    (Arg("pixels"), Arg("width"), Arg("height"),
-      //                     Arg("delay_num") = DEFAULT_FRAME_NUMERATOR, Arg("dela_den") = DEFAULT_FRAME_DENOMINATOR))
+      //                     Arg("delay_num") = DEFAULT_FRAME_NUMERATOR, Arg("delay_den") = DEFAULT_FRAME_DENOMINATOR))
       .define_method("pixels", &APNGFrame::pixels, (Arg("pixels") = NULL))
       .define_method("width", &APNGFrame::width, (Arg("width") = 0))
       .define_method("height", &APNGFrame::height, (Arg("height") = 0))
@@ -152,7 +153,7 @@ void Init_rapngasm()
       .define_method("transparency_size", &APNGFrame::transparencySize, (Arg("transparency_size") = NULL))
       .define_method("delay_numerator", &APNGFrame::delayNum, (Arg("delay_numerator") = 0))
       .define_method("delay_denominator", &APNGFrame::delayDen, (Arg("delay_denominator") = 0))
-      .define_method("rows", &APNGFrame::rows, (Arg("rows") = NULL))
+      .define_method("rows", &APNGFrame::rows, (Arg("rows") = NULL))      // TODO
       .define_method("save", &APNGFrame::save, (Arg("save")));
 
     define_class<APNGAsm>("APNGAsmSuper")
@@ -163,7 +164,7 @@ void Init_rapngasm()
       .define_method("save_pngs", &APNGAsm::savePNGs)
       .define_method("save_json", &APNGAsm::saveJson)
       .define_method("save_xml", &APNGAsm::saveXml)
-      //.define_method("set_apngasm_listener", &APNGAsm::setAPNGAsmListener)
+      //.define_method("set_apngasm_listener", &APNGAsm::setAPNGAsmListener)    // TODO
       .define_method("get_frames", &APNGAsm::getFrames)
       .define_method("frame_count", &APNGAsm::frameCount)
       .define_method("reset", &APNGAsm::reset)
