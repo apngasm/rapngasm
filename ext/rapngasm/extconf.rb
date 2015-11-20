af08920c80c8776aabe4fd52a653d609cf312958
影月 零 <zero@genshin.org>
2015-11-21 02:55:33 +0900

    adjust extconf to actually run precompilation gears

--- ext/rapngasm/extconf.rb
+++ ext/rapngasm/extconf.rb
@@ -4,12 +4,14 @@ ext_source_path = File.dirname(__FILE__)
 
 return unless have_library('stdc++')
 
+require_relative "#{ext_source_path}/../tools/prebuild"
+
 $stdout.write 'Checking for APNGAsm.'
 if have_library('apngasm')
   $stdout.write 'APNGAsm found, building RAPNGAsm.'
 else
-  $stdout.write 'APNGAsm NOT found, Attempting to build...'
-  require_relative "#{ext_source_path}/../tools/prebuild" 
+  $stdout.write 'APNGAsm NOT found. Please install libapngasm.'
+  exit 1
 end
 
 dir_config('rapngasm')
