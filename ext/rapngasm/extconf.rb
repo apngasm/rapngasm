cc0b032b727a2777f04ecc26aa0d6d55b44d43ea
影月 零 <zero@genshin.org>
2015-11-23 22:09:12 +0900

    get path

--- ext/rapngasm/extconf.rb
+++ ext/rapngasm/extconf.rb
@@ -15,7 +15,7 @@ $libs = append_library($libs, 'apngasm')
 dir_config('rapngasm')
 
 $stdout.write 'Generating native interface wrappers with SWIG'
-`swig -c++ -ruby apngasm.i`
+`swig -c++ -ruby #{__dir__}/apngasm.i`
 $stdout.write 'Wrappers generated'
 $distcleanfiles += ['apngasm_wrap.cxx']
 
