3253a0d375d3610cb77805d1dd0902945da2bf27
影月 零 <zero@genshin.org>
2015-12-01 15:57:06 +0900

    use regex to get header path dynamically

--- ext/rapngasm/extconf.rb
+++ ext/rapngasm/extconf.rb
@@ -10,12 +10,17 @@ else
   exit 1
 end
 
+#find header location
+header_loc = `echo '#include <apngasm-conf.h>' | cpp -H -o /dev/null 2>&1 | head -n1`
+header_loc = header_loc.match("\/.*\/")
+$stdout.write "\nAPNGAsm config header found in: #{header_loc}"
+
 dir_config('rapngasm')
 
 $stdout.write "\nGenerating native interface wrappers with SWIG\n"
 $stdout.write "Using SWIG from #{`which swig`}\n"
 $stdout.write "Generating wrapper sources from #{File.expand_path(File.dirname(__FILE__))}/apngasm.i\n"
-`swig -c++ -ruby -I/usr/include #{File.expand_path(File.dirname(__FILE__))}/apng.i`
+`swig -c++ -ruby -I#{header_loc} #{File.expand_path(File.dirname(__FILE__))}/apng.i`
 $stdout.write 'Wrappers generated'
 $distcleanfiles += ['apngasm_wrap.cxx']
 
